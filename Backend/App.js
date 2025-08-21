4d6a5ee506b0439c553e9f2bcbfb6e7947dca45e
Marwan-Mohammed45 <marwan.3zazy@gmail.com>
2025-08-22 01:37:55 +0300

    Vercel.json

--- Backend/App.js
+++ Backend/App.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { connectDB } from "./DB/connectdb.js";
 import patientAuthroutes from "./routes/patient.auth.routes.js";
 import doctorAuthroutes from "./routes/doctor.auth.routes.js";
-
 import cors from "cors";
 import medicalHistory from "./routes/MedicalHistory.Routes.js";
 
@@ -11,10 +10,10 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors());
 
-// ✅ Serve uploaded images
 app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
@@ -26,6 +25,10 @@ app.use("/medicalHistory", medicalHistory);
 
 connectDB();
 
-app.listen(port, () => {
-  console.log(`✅ Server is running on http://localhost:${port}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`✅ Server is running on http://localhost:${port}`);
+  });
+}
+
+export default app;
